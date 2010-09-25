f4f83cc2ba6be2da17eb45af27a0a18001a48a1c
Bernard Micallef <bernard.micallef@gmail.com>
2010-09-26 01:01:35 +0200

    Fixed embeds and removed unecessary code.

--- hma.js
+++ hma.js
@@ -3,12 +3,12 @@ function hma_load()
 	var hmehPro = '?hmeh-pro=';
 	var loc = window.location.href;
 	
-	var index = loc.indexOf(hmehPro);
+	var hmehIndex = loc.indexOf(hmehPro);
 	
-	if (index != -1)
+	if (hmehIndex != -1)
 	{
 		document.body.style.visibility = "hidden";
-		var vidId = loc.substr(index + hmehPro.length);
+		var vidId = loc.substr(hmehIndex + hmehPro.length);
 		
 		var frm = document.forms[0];
 		frm.elements[0].value = "http://www.youtube.com/watch?v=" + vidId;
@@ -16,16 +16,17 @@ function hma_load()
 	}
 	else
 	{
-		if (document.referrer.indexOf(hmehPro) == -1)
+		var ref = document.referrer;
+		var hmehIndexRef = ref.indexOf(hmehPro);
+		
+		if (hmehIndexRef == -1)
 		{
 			return;
-		}	
+		}
 		
 		if (document.getElementById('error') != null)
-		{			
-			var ref = document.referrer;
-			var indexRef = ref.indexOf(hmehPro);
-			window.vidIdRef = ref.substr(indexRef + hmehPro.length);
+		{
+			window.vidIdRef = ref.substr(hmehIndexRef + hmehPro.length);			
 			document.documentElement.innerHTML = "<div style='color:#fff; background-color:#000; text-align:center;'>Error, please <a style='color:#fff;' href='" + ref + "' onclick='window.parent.postMessage(vidIdRef + \"|HMEH|\" + \"HIDE\",\"*\");'>reload</a> the video.</div>";
 			window.parent.postMessage(vidIdRef + "|HMEH|" + "ERROR",'*');
 		}
@@ -40,32 +41,30 @@ function hma_load()
 				player.stopBuffering();
 				player.startBuffering();
 				player.show();
+				
+				window.parent.postMessage(vidId + "|HMEH|" + title,'*');
 			});
 			
 			var targetAcquired = "youtube.com/watch?v=";
-
 			var target = document.getElementById("hmainput").value;
 
-			var index = target.toLowerCase().indexOf(targetAcquired);
-			var vidId = target.substr(index + targetAcquired.length);
+			var vidId = target.substr(target.toLowerCase().indexOf(targetAcquired) + targetAcquired.length);
 
 			if (vidId.indexOf("&") != -1)
 			{
 				vidId = vidId.substr(0, vidId.indexOf("&"));
 			}
-			
+
 			var title = document.getElementById("eow-title").innerText;
-			
+
 			var video = document.getElementById('hma-player_api');
-			
-			var trans = document.createElement('param');
-			trans.setAttribute('name','wmode');
-			trans.setAttribute('value','transparent');
-			video.appendChild(trans);
-			
+
+			var playerFix = document.createElement('param');
+			playerFix.setAttribute('name','wmode');
+			playerFix.setAttribute('value','transparent');
+			video.appendChild(playerFix);
+
 			document.body.innerHTML = video.outerHTML;		
-			
-			window.parent.postMessage(vidId + "|HMEH|" + title,'*');
 		}
 	}
 }
